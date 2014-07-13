0b70fd1f530924703f5402f87b3404229912d0e8
hackiftekhar <hack.iftekhar@gmail.com>
2014-07-14 02:27:32 +0530

    Added icon, Enhance Demo

--- IQPhotoEditor Demo/IQPhotoEditor Demo/ViewController.m
+++ IQPhotoEditor Demo/IQPhotoEditor Demo/ViewController.m
@@ -8,17 +8,16 @@
 #import "ViewController.h"
 #import <IQPhotoEditor/IQPhotoEditor.h>
 
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@end
+
 @implementation ViewController
 {
     IBOutlet UIImageView *imageView;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
- 	// Do any additional setup after loading the view, typically from a nib.
-}
-
+#pragma mark - IQPhotoEditor
 - (IBAction)openPhotoEditorFromImageView:(UIButton *)sender
 {
     [self presentPhotoEditorForImageView:imageView];
@@ -30,6 +29,50 @@
     [self presentPhotoEditor:controller presentationStyle:IQPhotoEditorPresentationStyleDefault withCompletion:^(UIImage *image, BOOL isModified) {
         imageView.image = image;
     }];
+}
+
+#pragma mark - Save Action
+- (IBAction)saveAction:(UIBarButtonItem *)barButton
+{
+    if (imageView.image)
+    {
+        UIImageWriteToSavedPhotosAlbum(imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+    }
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    if (error)
+    {
+        [[[UIAlertView alloc] initWithTitle:@"Error!" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] show];
+    }
+    else
+    {
+        [[[UIAlertView alloc] initWithTitle:@"Success!" message:@"Photo saved to Photo Album" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] show];
+    }
+}
+
+#pragma mark - Open Photo Library
+- (IBAction)tapAction:(UITapGestureRecognizer *)gesture
+{
+    if (gesture.state == UIGestureRecognizerStateEnded)
+    {
+        UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+        controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        controller.delegate = self;
+        [self presentViewController:controller animated:YES completion:nil];
+    }
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    imageView.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
